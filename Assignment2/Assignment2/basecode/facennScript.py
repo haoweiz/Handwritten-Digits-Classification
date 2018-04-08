f91a893f7eb7b68a608dda31255ae057b096815f
os-class <haoweizh@buffalo.edu>
2018-04-08 02:24:07 -0400

    finish_facennScript

--- Assignment2/Assignment2/basecode/facennScript.py
+++ Assignment2/Assignment2/basecode/facennScript.py
@@ -4,6 +4,8 @@ Comparing single layer MLP with deep MLP (using TensorFlow)
 
 import numpy as np
 import pickle
+from scipy.optimize import minimize
+from math import sqrt
 
 # Do not change this
 def initializeWeights(n_in,n_out):
@@ -26,11 +28,67 @@ def initializeWeights(n_in,n_out):
 # Replace this with your sigmoid implementation
 def sigmoid(z):
 # Replace this with your nnObjFunction implementation
+    return  1.0 / (1.0 + np.exp(-z))
 def nnObjFunction(params, *args):
+    n_input, n_hidden, n_class, training_data, training_label, lambdaval = args
+    # 716    50        10       (50000,716)    (50000,)        0
+
+    w1 = params[0:n_hidden * (n_input + 1)].reshape((n_hidden, (n_input + 1)))
+    w2 = params[(n_hidden * (n_input + 1)):].reshape((n_class, (n_hidden + 1)))
+    obj_val = 0
+    # w1:(50,717), w2:(10,51)
+
+    # Your code here
+    row = training_data.shape[0]         #50000
+    column = training_data.shape[1]      #716
+    number_w1 = w1.shape[0]              #50
+    number_w2 = w2.shape[0]              #10
+
+    # Feedforward
+    from_input = np.concatenate((training_data,np.ones(shape = (row,1))),1) #(50000,717)
+    out_hidden = sigmoid(w1.dot(np.transpose(from_input)))                  #(50,717) * (717,50000) = (50,50000)
+    from_hidden = np.concatenate((out_hidden,np.ones(shape = (1,row))),0)   #(51,50000)
+    out = sigmoid(w2.dot(from_hidden))                                      #(10,51) * (51,50000) = (10,50000)
+
+    # BackPropagation
+    new_training_label = np.zeros((n_class,row))  # (10,50000), Transform training_label to 0/1 form.
+    for i in range(0,row):
+        new_training_label[(int)(training_label[i])][i] = 1      # (50000,10), Set the correspond label to 1.
     
+    obj_val_front = 0 - ((1/row) * (np.sum(np.multiply(new_training_label,np.log(out)) + np.multiply(np.subtract(1,new_training_label), np.log(np.subtract(1,out))))))
+    obj_val_back = np.multiply((lambdaval/(2*row)), (np.sum(np.square(w1)) + np.sum(np.square(w2))))
+    obj_val = obj_val_front + obj_val_back
+    
+    # print (obj_val)
+
+    delta_l = np.subtract(out,new_training_label)       #(10,50000)
+    grad_w2 = np.multiply(1/(row), np.dot(delta_l,np.transpose(from_hidden)) + np.multiply(lambdaval,w2))
+
+    a = np.multiply(np.subtract(1,out_hidden), out_hidden)        #(50,50000)
+    w2_nobias = np.delete(w2,w2.shape[1]-1, axis = 1)             #(10,50)
+    b = np.multiply(np.transpose(a), np.dot(np.transpose(delta_l), w2_nobias))  #(50000,50) * (50000,50)
+    c = np.concatenate((training_data,np.ones(shape = (row,1))),axis = 1) #(50000,717)
+    grad_w1 = np.multiply(1/(row),np.dot(np.transpose(b), c) + np.multiply(lambdaval, w1))
+
+    # Make sure you reshape the gradient matrices to a 1D array. for instance if your gradient matrices are grad_w1 and grad_w2
+    # you would use code similar to the one below to create a flat array
+    obj_grad = np.array([])
+    obj_grad = np.concatenate((grad_w1.flatten(), grad_w2.flatten()),0)
+    # obj_grad = np.array([])
+
+    return (obj_val, obj_grad)
 # Replace this with your nnPredict implementation
 def nnPredict(w1,w2,data):
-
+    # w1:(50,715)  w2:(10,51)  data:(10000,714)
+    labels = np.array([])
+    #Your code here
+    data_bias = np.concatenate((data, np.ones((data.shape[0], 1))),1)    #(10000,715)
+    out_hidden = sigmoid(data_bias.dot(np.transpose(w1)));       #(10000,715) * (715,50) = (10000,50)
+    out_hidden_bias = np.concatenate((out_hidden ,np.ones((data.shape[0],1))),1);  #(10000,51)
+    out = sigmoid(np.dot(out_hidden_bias,np.transpose(w2)));     #(10000,51) * (51,10) = (10000,10)
+    
+    labels = np.argmax(out,1)
+    return labels
 # Do not change this
 def preprocess():
     pickle_obj = pickle.load(file=open('face_all.pickle', 'rb'))
